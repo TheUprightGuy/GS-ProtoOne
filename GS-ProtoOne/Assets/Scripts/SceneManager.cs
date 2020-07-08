c1f74d088aee86a030a775bab44cbe5f9c72e306
Jacob Gallagher <jacob.gal8369@mediadesign.school.nz>
2020-07-08 20:36:12 +1200

    Basic Menu with Controller input

---
+++ GS-ProtoOne/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField] private string customisationSceneName = "DefaultTransitionScene";
+    public void LoadCustomisationScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(customisationSceneName);
+    }
+    
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
