c2222433e1e60c817965cf4a2bdd2db51396eccb
BigGriz <waydbarton@hotmail.co.nz>
2020-07-08 20:38:30 +1200

    PartSwapper UI Base Setup

---
+++ GS-ProtoOne/Assets/AbilitiesUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilitiesUI : MonoBehaviour
+{
+    public Image icon;
+    public TMPro.TextMeshProUGUI text;
+
+    public void SwitchPrefab(Part _part)
+    {
+        icon.sprite = _part.abilitySprite;
+        text.SetText(_part.abilityText);
+    }
+}
