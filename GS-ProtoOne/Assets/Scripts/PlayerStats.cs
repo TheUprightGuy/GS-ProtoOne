9e18d6fccfb6615b850c016bbf6333898a97fe6e
BigGriz <waydbarton@hotmail.co.nz>
2020-07-08 21:28:37 +1200

    Canvas Showing Player

---
+++ GS-ProtoOne/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Body
+{
+    public Part head;
+    public Part arms;
+    public Part legs;
+}
+
+public class PlayerStats : MonoBehaviour
+{
+    public Body body;
+}
