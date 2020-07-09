526596c917cc857e9a8616f28e0cc201fc9951d4
BigGriz <waydbarton@hotmail.co.nz>
2020-07-09 22:28:29 +1200

    Charge Test

---
+++ GS-ProtoOne/Assets/Scripts/Testing/TestScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public bool charging = false;
+    public Rigidbody rb;
+    public float speed = 0.5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            charging = true;
+        }
+
+        if (charging)
+        {
+            rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+        }
+    }
+    //private void FixedUpdate()
+    //{
+    //    if (charging)
+    //    {
+    //        rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+    //    }
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        charging = false;
+    }
+}
