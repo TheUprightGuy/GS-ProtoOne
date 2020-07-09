5c9028618456f8732cd9e6a01a51ac229fb33491
BigGriz <waydbarton@hotmail.co.nz>
2020-07-09 12:42:32 +1200

    Carrying Stats

---
+++ GS-ProtoOne/Assets/Scripts/EventHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Move this to event handler in future
+using UnityEngine.SceneManagement;
+
+public class EventHandler : MonoBehaviour
+{
+    public static EventHandler instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one EventHandler exists!");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public PartPicker pp1;
+    public PartPicker pp2;
+
+
+    private void Update()
+    {
+
+    }
+
+
+    // Start Button is Pressed
+    public event Action<int> readyUp;
+    public void ReadyUp(int _id)
+    {
+        if (readyUp != null)
+        {
+            readyUp(_id);
+        }
+    }
+
+    public event Action setupPlayers;
+    public void SetupPlayers()
+    {
+        if (setupPlayers != null)
+        {
+            setupPlayers();
+        }
+    }
+}
---
+++ GS-ProtoOne/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Body
+{
+    public Part head;
+    public Part arms;
+    public Part legs;
+}
+
+public class PlayerStats : MonoBehaviour
+{
+    public Body body;
+    public int id;
+    public Rigidbody rb;
+
+    // Just for Debugging atm - check its working
+    public Part head;
+    public Part arms;
+    public Part legs;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Setup()
+    {
+        head = body.head;
+        arms = body.arms;
+        legs = body.legs;
+
+        rb.useGravity = true;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Start"))
+        {
+            // Ready Up
+            EventHandler.instance.ReadyUp(id);
+        }
+    }
+}
