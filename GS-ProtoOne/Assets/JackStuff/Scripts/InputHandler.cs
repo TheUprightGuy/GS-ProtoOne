57dee6c83528f511e0061d8871bd5517764e5eb7
Jack <jack.belton@hotmail.com>
2020-07-10 17:34:03 +1200

    Multiplayer handler

---
+++ GS-ProtoOne/Assets/JackStuff/Scripts/InputHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.InputSystem.LowLevel;
+
+[System.Serializable]
+public struct InputData
+{
+    public string ControlScheme;
+    public InputDevice RegisteredDevice; 
+}
+
+public class InputHandler : MonoBehaviour
+{
+    //public PlayerInput playerControls;
+
+    /// <summary>
+        /// 0 Index indicates setting to none.
+    /// </summary>
+    public int QueryIndex = 0;
+
+    public InputData[] AllPlayers;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        InputSystem.onEvent += QueryAnyInput;
+    }
+
+    // Update is called once per frame
+    public void QueryInputIndex(int _iIndex)
+    {
+        QueryIndex = _iIndex;
+    }
+
+    void QueryAnyInput(InputEventPtr eventPtr, InputDevice device)
+    {
+        
+        // Ignore anything that isn't a state event.
+        if (!eventPtr.IsA<StateEvent>() && !eventPtr.IsA<DeltaStateEvent>() || QueryIndex == 0)
+            return;
+
+        if (QueryIndex > AllPlayers.Length)
+        {
+            Debug.LogWarning("QueryIndex greater than player count");
+        }
+        var gamepad = device as Gamepad;
+        var keyboard = device as Keyboard;
+
+
+        if (gamepad != null)
+        {
+            //Debug.Log("Gamepad with ID of " + gamepad.deviceId.ToString());
+            AllPlayers[QueryIndex - 1].ControlScheme = "XboxController";
+
+            
+        }
+        else if (keyboard != null)
+        {
+            //Debug.Log("KeyBoard" );
+            AllPlayers[QueryIndex - 1].ControlScheme = "KeyBoard";
+        }
+        else
+        {
+            return;
+        }
+
+        AllPlayers[QueryIndex - 1].RegisteredDevice = device;
+        QueryIndex = 0;
+    }
+
+
+    
+}
---
+++ GS-ProtoOne/Assets/JackStuff/Scripts/SetInput.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(PlayerInput))]
+public class SetInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool refreshInputs = true;
+    public int playerNo = 0;
+    private GameObject InputHandler;
+    private PlayerInput thisInput;
+    void Start()
+    {
+        thisInput = GetComponent<PlayerInput>();
+        thisInput.enabled = false;
+        InputHandler = GameObject.FindGameObjectWithTag("InputController");
+        if (InputHandler == null)
+        {
+            Debug.LogError("InputsHandler not found");
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (refreshInputs)
+        {
+            refreshInputs = false;
+            pullPlayerInfo();
+        }
+    }
+
+    private bool pullPlayerInfo()
+    {
+        InputData playerData = InputHandler.GetComponent<InputHandler>().AllPlayers[playerNo - 1];
+        if (playerData.ControlScheme == "")
+        {
+            Debug.LogError("Player " + playerNo.ToString() + " ControlScheme not found");
+            return false;
+        }
+        if (playerData.RegisteredDevice == null)
+        {
+            Debug.LogError("Player " + playerNo.ToString() + " RegisteredDevice not found");
+            return false;
+        }
+        
+        thisInput.enabled = true;
+        thisInput.SwitchCurrentControlScheme(playerData.ControlScheme, playerData.RegisteredDevice);
+
+        return true;
+    }
+}
