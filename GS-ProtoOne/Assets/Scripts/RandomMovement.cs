d27f808f9ae0d1d6df269b682f50817cfcd5e923
Jacob Gallagher <jacob.gal8369@mediadesign.school.nz>
2020-07-13 16:53:47 +1200

    Fancy camera movement in menu

--- GS-ProtoOne/Assets/Scripts/RandomMovement.cs
+++ GS-ProtoOne/Assets/Scripts/RandomMovement.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.PlayerLoop;
 
 public class RandomMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxRangeFromCenter = 15;
+    public float timeToComplete = 30;
+    public float rollStrength = 5;
+
+    private LookAtConstraint _lookAtConstraint;
+    private void Awake()
     {
-        
+        _lookAtConstraint = this.GetComponent<LookAtConstraint>();
+        rollStrength *= -1;
+        _lookAtConstraint.roll = rollStrength;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(LeanTween.isTweening(this.gameObject)) return;
+        LeanTween.moveX(this.gameObject, maxRangeFromCenter, timeToComplete).setEaseInOutSine();
+        LeanTween.value(this.gameObject, UpdateRollValue, rollStrength, rollStrength * -1, timeToComplete);
+        maxRangeFromCenter *= -1;
+        rollStrength *= -1;
+    }
+
+    private void UpdateRollValue(float val)
+    {
+        _lookAtConstraint.roll = val;
     }
 }
