b6e59d2289424243d2c79f2195af574ca86ea66b
Jack Belton <jack.belton@hotmail.com>
2020-07-15 13:06:47 +1200

    Started on hits

---
+++ GS-ProtoOne/Assets/Scripts/Player/Hit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+
+    public float HitCooldown = 0.1f;
+    SphereCollider colliderComp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliderComp = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    // }
+    // private void OnTriggerEnter(Collider other) {
+    //     if (other.transform.parent.tag == "Player")
+    //     {
+    //         colliderComp.enabled = false;
+    //         Debug.Log("Hit");
+    //         Invoke("HitCoolDown", HitCooldown);
+    //     }
+    //
+    }
+
+
+    public void HitSwitch()
+    {
+        colliderComp.enabled = !colliderComp.enabled;
+    }
+}
---
+++ GS-ProtoOne/Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public float HitCooldown = 0.5f;
+    private GameObject hitCollider = null;
+    private void OnTriggerEnter(Collider other) {
+      
+    }
+
+private void OnCollisionEnter(Collision other) {
+      if (other.gameObject.tag == "Hit")
+        {
+            Debug.Log("Hit");
+            GetComponent<BoxCollider>().enabled = false;
+            Invoke("HitboxHit", HitCooldown);
+
+        }
+}
+    private void HitBoxHit()
+    {
+        GetComponent<BoxCollider>().enabled = true;
+    }
+}
