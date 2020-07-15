37212be5376da35b0011dfb35d4ebde19afd4538
Jack Belton <jack.belton@hotmail.com>
2020-07-15 15:44:47 +1200

    Damaging, blocking

--- GS-ProtoOne/Assets/Scripts/Player/PlayerCombat.cs
+++ GS-ProtoOne/Assets/Scripts/Player/PlayerCombat.cs
@@ -7,14 +7,21 @@ public class PlayerCombat : MonoBehaviour
     public float HitCooldown = 0.5f;
     private GameObject hitCollider = null;
 
+
+    
+    private PlayerStats thisStats;
+
+    public float blockTopY = 0.0f;
+    public float blockBottomY = 1.0f;
+
+
+
+    [Header("Colliders")]
     public SphereCollider LeftHandCarpals;
     public SphereCollider RightHandCarpals;
 
     public SphereCollider LeftFootLateralCuneiform;
     public SphereCollider RightFootLateralCuneiform;
-
-    public PlayerStats thisStats;
-
     private void Awake() {
         thisStats = GetComponent<PlayerStats>();
     }
@@ -43,14 +50,23 @@ public class PlayerCombat : MonoBehaviour
         RightHandCarpals.enabled = false;
 
     }
+
+    private Vector3 hitPos = Vector3.zero;
     public void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Hit")
         {
+            hitPos = other.transform.position;
             thisStats.TakeDamage(1.0f);
             Debug.Log("zoop");
         }
     }
 
+    private void OnDrawGizmos() {
 
 
+        float calcTopY = transform.position.y + blockTopY;
+        float calcBotY = transform.position.y + blockBottomY;
+        Gizmos.color = ((hitPos.y < calcTopY) && (hitPos.y > calcBotY)) ? (Color.red) : (Color.white);
+        Gizmos.DrawSphere(hitPos, 0.1f);
+    }
 }
