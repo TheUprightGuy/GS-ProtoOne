ad5d0d9742b110d6504c8b7b76aaf6119d91d1e7
BigGriz <waydbarton@hotmail.co.nz>
2020-07-16 18:04:42 +1200

    TimeOut Round - Need to Add Draw Later

--- GS-ProtoOne/Assets/Scripts/UI/HealthScript.cs
+++ GS-ProtoOne/Assets/Scripts/UI/HealthScript.cs
@@ -8,6 +8,7 @@ public class HealthScript : MonoBehaviour
     [Header("Setup Fields")]
     public int id;
     private Image healthBar;
+    [HideInInspector] public float health;
 
     #region Setup
     private void Start()
@@ -26,6 +27,7 @@ public class HealthScript : MonoBehaviour
     {
         if (id == _id)
         {
+            health = _health;
             healthBar.fillAmount = _health;
         }
     }
---
+++ GS-ProtoOne/Assets/TimerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float timer = 60;
+    //private startCount;
+    private TMPro.TextMeshProUGUI text;
+
+    public HealthScript p1;
+    public HealthScript p2;
+
+    public bool called = false;
+
+    private void Start()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        text.SetText(((int)timer).ToString());
+
+        if (timer <= 0 && !called)
+        {
+            called = true;
+            if (p1.health > p2.health)
+            {
+                EventHandler.instance.GameOver(1);
+            }
+            else if (p2.health > p1.health)
+            {
+                EventHandler.instance.GameOver(2);
+            }
+        }
+    }
+}
